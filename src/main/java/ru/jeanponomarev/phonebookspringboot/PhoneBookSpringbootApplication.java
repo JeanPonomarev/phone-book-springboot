dc97aa843e899dbdea93b06e43cb1a8997e47d02
Jean Ponomarev <Jean.Ponomarev@nordigy.ru>
2021-04-08 12:45:19 +0300

    Initial commit

---
+++ src/main/java/ru/jeanponomarev/phonebookspringboot/PhoneBookSpringbootApplication.java
@@ -0,0 +1,13 @@
+package ru.jeanponomarev.phonebookspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneBookSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneBookSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/jeanponomarev/phonebookspringboot/PhoneBookSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.jeanponomarev.phonebookspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneBookSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
