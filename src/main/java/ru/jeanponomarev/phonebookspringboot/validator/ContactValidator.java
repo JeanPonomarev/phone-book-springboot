2e67114cf29f9c0e431704f6cb6648850d40c01f
Jean Ponomarev <Jean.Ponomarev@nordigy.ru>
2021-04-15 13:56:07 +0300

    Server-side validation has been improved

--- src/main/java/ru/jeanponomarev/phonebookspringboot/validator/ContactValidator.java
+++ src/main/java/ru/jeanponomarev/phonebookspringboot/validator/ContactValidator.java
@@ -6,6 +6,8 @@ import ru.jeanponomarev.phonebookspringboot.dao.ContactDao;
 import ru.jeanponomarev.phonebookspringboot.entity.Contact;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Component
 public class ContactValidator {
@@ -22,9 +24,11 @@ public class ContactValidator {
 
         String phoneNumber = contact.getPhoneNumber();
 
-        if (isContactWithTargetPhoneNumberExistent(phoneNumber)) {
+        List<Contact> contactList = contactDao.getByPhoneNumber(phoneNumber);
+
+        if (!contactList.isEmpty()) {
             contactValidationResult.setValid(false);
-            contactValidationResult.setMessage(String.format("Contact with phone number \"%s\" already exist", phoneNumber));
+            contactValidationResult.setMessage(String.format("Contact with phone number \"%s\" already exists", phoneNumber));
 
             return contactValidationResult;
         }
@@ -33,7 +37,21 @@ public class ContactValidator {
     }
 
     public ContactValidationResult validateUpdateContact(Contact contact) {
-        return validateContactFields(contact);
+
+        ContactValidationResult contactValidationResult = validateContactFields(contact);
+
+        String phoneNumber = contact.getPhoneNumber();
+
+        List<Contact> contactList = contactDao.getByPhoneNumber(phoneNumber);
+
+        if (contactList.size() > 1) {
+            contactValidationResult.setValid(false);
+            contactValidationResult.setMessage(String.format("Contact with phone number \"%s\" already exists", phoneNumber));
+
+            return contactValidationResult;
+        }
+
+        return contactValidationResult;
     }
 
     private ContactValidationResult validateContactFields(Contact contact) {
@@ -53,6 +71,16 @@ public class ContactValidator {
             return contactValidationResult;
         }
 
+        Pattern firstNamePattern = Pattern.compile("(?i)(^[a-z])((?![ .,'-]$)[a-z .,'-]){1,20}$");
+        Matcher firstNameMatcher = firstNamePattern.matcher(contact.getFirstName());
+
+        if (!firstNameMatcher.matches()) {
+            contactValidationResult.setValid(false);
+            contactValidationResult.setMessage("First name is incorrect");
+
+            return contactValidationResult;
+        }
+
         if (contact.getLastName() == null) {
             contactValidationResult.setValid(false);
             contactValidationResult.setMessage("Last name shouldn't be null value");
@@ -63,6 +91,16 @@ public class ContactValidator {
         if (contact.getLastName().isEmpty()) {
             contactValidationResult.setValid(false);
             contactValidationResult.setMessage("Last name shouldn't be an empty string");
+
+            return contactValidationResult;
+        }
+
+        Pattern lastNamePattern = Pattern.compile("(?i)(^[a-z])((?![ .,'-]$)[a-z .,'-]){1,20}$");
+        Matcher lastNameMatcher = lastNamePattern.matcher(contact.getLastName());
+
+        if (!lastNameMatcher.matches()) {
+            contactValidationResult.setValid(false);
+            contactValidationResult.setMessage("Last name is incorrect");
 
             return contactValidationResult;
         }
@@ -81,14 +119,18 @@ public class ContactValidator {
             return contactValidationResult;
         }
 
+        Pattern phonePattern = Pattern.compile("([-()_.+ ]*\\d[-()_.+ ]*){4,}");
+        Matcher phoneMatcher = phonePattern.matcher(contact.getPhoneNumber());
+
+        if (!phoneMatcher.matches()) {
+            contactValidationResult.setValid(false);
+            contactValidationResult.setMessage("Phone number is incorrect");
+
+            return contactValidationResult;
+        }
+
         contactValidationResult.setValid(true);
 
         return contactValidationResult;
-    }
-
-    private boolean isContactWithTargetPhoneNumberExistent(String phoneNumber) {
-        List<Contact> contactList = contactDao.getByPhoneNumber(phoneNumber);
-
-        return !contactList.isEmpty();
     }
 }
--- src/test/java/ru/jeanponomarev/phonebookspringboot/validator/ContactValidatorTest.java
+++ src/test/java/ru/jeanponomarev/phonebookspringboot/validator/ContactValidatorTest.java
@@ -6,12 +6,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import ru.jeanponomarev.phonebookspringboot.dao.ContactDaoImpl;
 import ru.jeanponomarev.phonebookspringboot.entity.Contact;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.stream.Stream;
 
@@ -40,6 +42,50 @@ class ContactValidatorTest {
         assertThat(contactValidationResult.isValid()).isTrue();
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"Alexander", "Jo", "jason", "Aaaaaaaaaaaaaaaaaaaa"})
+    void shouldValidateCreateContactWithVariousFirstNameTypes(String firstName) {
+        Contact contact = new Contact(firstName, "Doe", "1234567");
+
+        Mockito.when(contactDao.getByPhoneNumber(Mockito.anyString())).thenReturn(Collections.emptyList());
+
+        ContactValidationResult contactValidationResult = contactValidator.validateCreateContact(contact);
+        assertThat(contactValidationResult.isValid()).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Ponomarev", "Jo", "smith", "Aaaaaaaaaaaaaaaaaaaa", "Sausage-Hausen", "d'Arras"})
+    void shouldValidateCreateContactWithVariousLastNameTypes(String lastName) {
+        Contact contact = new Contact("John", lastName, "1234567");
+
+        Mockito.when(contactDao.getByPhoneNumber(Mockito.anyString())).thenReturn(Collections.emptyList());
+
+        ContactValidationResult contactValidationResult = contactValidator.validateCreateContact(contact);
+        assertThat(contactValidationResult.isValid()).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"3469755", "346-97-55", "89134564322", "8-913-471-4748", "+79137436399", "+7-913-743-6399", "8-(383)-234-4534", "346 97 55", "1234"})
+    void shouldValidateCreateContactWithVariousPhoneNumberTypes(String phoneNumber) {
+        Contact contact = new Contact("John", "Doe", phoneNumber);
+
+        Mockito.when(contactDao.getByPhoneNumber(phoneNumber)).thenReturn(Collections.emptyList());
+
+        ContactValidationResult contactValidationResult = contactValidator.validateCreateContact(contact);
+        assertThat(contactValidationResult.isValid()).isTrue();
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideInvalidContacts")
+    void shouldValidateCreateContactWithInvalidField(Contact invalidContact, String expectedValidationMessage) {
+        Mockito.when(contactDao.getByPhoneNumber(invalidContact.getPhoneNumber())).thenReturn(Collections.emptyList());
+
+        ContactValidationResult contactValidationResult = contactValidator.validateCreateContact(invalidContact);
+
+        assertThat(contactValidationResult.isValid()).isFalse();
+        assertThat(contactValidationResult.getMessage()).isEqualTo(expectedValidationMessage);
+    }
+
     @Test
     void shouldValidateCreateContactWithExistentPhoneNumber() {
         String phoneNumber = "1234567";
@@ -55,32 +101,46 @@ class ContactValidatorTest {
 
     @Test
     void shouldValidateUpdateContact() {
+        String phoneNumber = "1234567";
         Contact contact = new Contact("John", "Doe", "1234567");
+
+        Mockito.when(contactDao.getByPhoneNumber(phoneNumber)).thenReturn(Collections.singletonList(new Contact()));
 
         ContactValidationResult contactValidationResult = contactValidator.validateUpdateContact(contact);
         assertThat(contactValidationResult.isValid()).isTrue();
     }
 
-    private static Stream<Arguments> provideInvalidContacts() {
-        return Stream.of(
-            Arguments.of(new Contact(null, "Doe", "1234567"), "First name shouldn't be null value"),
-            Arguments.of(new Contact("", "Doe", "1234567"), "First name shouldn't be an empty string"),
-            Arguments.of(new Contact("John", null, "1234567"), "Last name shouldn't be null value"),
-            Arguments.of(new Contact("John", "", "1234567"), "Last name shouldn't be an empty string"),
-            Arguments.of(new Contact("John", "Doe", null), "Phone number shouldn't be null value"),
-            Arguments.of(new Contact("John", "Doe", ""), "Phone number shouldn't be an empty string")
-        );
+    @ParameterizedTest
+    @ValueSource(strings = {"Alexander", "Jo", "jason", "Aaaaaaaaaaaaaaaaaaaa"})
+    void shouldValidateUpdateContactWithVariousFirstNameTypes(String firstName) {
+        Contact contact = new Contact(firstName, "Doe", "1234567");
+
+        Mockito.when(contactDao.getByPhoneNumber(Mockito.anyString())).thenReturn(Collections.singletonList(new Contact()));
+
+        ContactValidationResult contactValidationResult = contactValidator.validateUpdateContact(contact);
+        assertThat(contactValidationResult.isValid()).isTrue();
     }
 
     @ParameterizedTest
-    @MethodSource("provideInvalidContacts")
-    void shouldValidateCreateContactWithInvalidField(Contact invalidContact, String expectedValidationMessage) {
-        Mockito.when(contactDao.getByPhoneNumber(invalidContact.getPhoneNumber())).thenReturn(Collections.emptyList());
+    @ValueSource(strings = {"Ponomarev", "Jo", "smith", "Aaaaaaaaaaaaaaaaaaaa", "Sausage-Hausen", "d'Arras"})
+    void shouldValidateUpdateContactWithVariousLastNameTypes(String lastName) {
+        Contact contact = new Contact("John", lastName, "1234567");
 
-        ContactValidationResult contactValidationResult = contactValidator.validateCreateContact(invalidContact);
+        Mockito.when(contactDao.getByPhoneNumber(Mockito.anyString())).thenReturn(Collections.singletonList(new Contact()));
 
-        assertThat(contactValidationResult.isValid()).isFalse();
-        assertThat(contactValidationResult.getMessage()).isEqualTo(expectedValidationMessage);
+        ContactValidationResult contactValidationResult = contactValidator.validateUpdateContact(contact);
+        assertThat(contactValidationResult.isValid()).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"3469755", "346-97-55", "89134564322", "8-913-471-4748", "+79137436399", "+7-913-743-6399", "8-(383)-234-4534", "346 97 55", "1234"})
+    void shouldValidateUpdateContactWithVariousPhoneNumberTypes(String phoneNumber) {
+        Contact contact = new Contact("John", "Doe", phoneNumber);
+
+        Mockito.when(contactDao.getByPhoneNumber(phoneNumber)).thenReturn(Collections.emptyList());
+
+        ContactValidationResult contactValidationResult = contactValidator.validateUpdateContact(contact);
+        assertThat(contactValidationResult.isValid()).isTrue();
     }
 
     @ParameterizedTest
@@ -90,5 +150,40 @@ class ContactValidatorTest {
 
         assertThat(contactValidationResult.isValid()).isFalse();
         assertThat(contactValidationResult.getMessage()).isEqualTo(expectedValidationMessage);
+    }
+
+    @Test
+    void shouldValidateUpdateContactWithTakenPhoneNumber() {
+        String phoneNumber = "1234567";
+        Contact contact = new Contact("John", "Doe", phoneNumber);
+
+        Mockito.when(contactDao.getByPhoneNumber(phoneNumber)).thenReturn(Arrays.asList(new Contact(), new Contact()));
+
+        ContactValidationResult contactValidationResult = contactValidator.validateUpdateContact(contact);
+
+        assertThat(contactValidationResult.isValid()).isFalse();
+        assertThat(contactValidationResult.getMessage()).isEqualTo(String.format("Contact with phone number \"%s\" already exist", phoneNumber));
+    }
+
+    private static Stream<Arguments> provideInvalidContacts() {
+        return Stream.of(
+                Arguments.of(new Contact(null, "Doe", "1234567"), "First name shouldn't be null value"),
+                Arguments.of(new Contact("", "Doe", "1234567"), "First name shouldn't be an empty string"),
+                Arguments.of(new Contact("2423423", "Doe", "1234567"), "First name is incorrect"),
+                Arguments.of(new Contact("I", "Doe", "1234567"), "First name is incorrect"),
+                Arguments.of(new Contact("Aaaaaaaaaaaaaaaaaaaaaa", "Doe", "1234567"), "First name is incorrect"),
+
+                Arguments.of(new Contact("John", null, "1234567"), "Last name shouldn't be null value"),
+                Arguments.of(new Contact("John", "", "1234567"), "Last name shouldn't be an empty string"),
+                Arguments.of(new Contact("John", "12345", "1234567"), "Last name is incorrect"),
+                Arguments.of(new Contact("John", "I", "1234567"), "Last name is incorrect"),
+                Arguments.of(new Contact("John", "Aaaaaaaaaaaaaaaaaaaaaa", "1234567"), "Last name is incorrect"),
+
+                Arguments.of(new Contact("John", "Doe", null), "Phone number shouldn't be null value"),
+                Arguments.of(new Contact("John", "Doe", ""), "Phone number shouldn't be an empty string"),
+                Arguments.of(new Contact("John", "Doe", "abcde"), "Phone number is incorrect"),
+                Arguments.of(new Contact("John", "Doe", "123"), "Phone number is incorrect"),
+                Arguments.of(new Contact("John", "Doe", "123abc"), "Phone number is incorrect")
+        );
     }
 }
